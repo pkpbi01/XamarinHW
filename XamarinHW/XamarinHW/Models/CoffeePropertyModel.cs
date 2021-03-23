7907110a7c2c7943b4ed3c0a75aae58556acdab8
first <pkpbi01@mail.ru>
2021-03-23 16:29:49 +0300

    CollectionView
    finish work with binding

--- XamarinHW/XamarinHW/Models/CoffeePropertyModel.cs
+++ XamarinHW/XamarinHW/Models/CoffeePropertyModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace XamarinHW.Models
 {
--- XamarinHW/XamarinHW/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ XamarinHW/XamarinHW/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace XamarinHW {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.CollectionView CollectionView;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            CollectionView = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.CollectionView>(this, "CollectionView");
         }
     }
 }
